8f1fddefabd1cbef060c14572bea7ec894ae53ad
Yaroslav <iaroslav.bierioza@gmail.com>
2025-10-29 15:16:56 +0300

    create database config

--- example-base-app/core/config.py
+++ example-base-app/core/config.py
@@ -1,4 +1,4 @@
-from pydantic import BaseModel
+from pydantic import BaseModel, PostgresDsn
 from pydantic_settings import BaseSettings
 
 
@@ -6,8 +6,17 @@ class ApiPrefix(BaseModel):
     prefix: str = "/api"
 
 
+class DataBaseConfig(BaseModel):
+    url: PostgresDsn
+    echo: bool = False
+    echo_pool: bool = False
+    pool_size: int = 50
+    max_overflow: int = 10
+
+
 class Settings(BaseSettings):
     api: ApiPrefix = ApiPrefix()
+    db: DataBaseConfig
 
 
 settings = Settings()

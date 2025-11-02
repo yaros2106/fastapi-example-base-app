271e513b03c83abd6c2dc991b99d0f750af2b04b
Yaroslav <iaroslav.bierioza@gmail.com>
2025-11-02 13:30:24 +0300

    create declarative Base

--- example-base-app/core/models/__init__.py
+++ example-base-app/core/models/__init__.py
@@ -1,4 +1,8 @@
-__all__ = ("db_helper",)
+__all__ = (
+    "db_helper",
+    "Base",
+)
 
 
 from .db_helper import db_helper
+from .base import Base
---
+++ example-base-app/core/models/base.py
@@ -0,0 +1,18 @@
+from sqlalchemy.orm import (
+    DeclarativeBase,
+    Mapped,
+    mapped_column,
+    declared_attr,
+)
+
+from utils import camel_case_to_snake_case
+
+
+class Base(DeclarativeBase):
+    __abstract__ = True
+
+    @declared_attr
+    def __tablename__(cls) -> str:
+        return f"{camel_case_to_snake_case(cls.__name__)}s"
+
+    id: Mapped[int] = mapped_column(primary_key=True)
